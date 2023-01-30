0b85507e4e807a32bad4af3748d5ab0582de88e5
LoveSy <shana@zju.edu.cn>
2023-01-30 20:09:49 +0800

    Github pages

--- helper/build.gradle.kts
+++ helper/build.gradle.kts
@@ -26,6 +26,13 @@ android {
         }
     }
 
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
